564b66e2e887bdec0cc2750c4e4ba8e8fdd9792f
Matt Strimas-Mackey <mstrimas@gmail.com>
2018-01-17 09:54:42 -0500

    si2

--- R/generate-nsf.r
+++ R/generate-nsf.r
@@ -51,7 +51,7 @@
 #' neighbour <- generate_nsf(pus, buffer = 20, locked = "locked_out")
 #'
 #' # neighbouring solution
-#' print(devtools::session_info())
+#' print(sessionInfo())
 #' neighbour(selected)
 #' neighbour(selected, pu_list = TRUE)
 generate_nsf <- function(pu, buffer, locked, recalculate = 1L) {
